d55083229bf3336a0776be1b0855de8f42211551
tushargg-gif <62676988+tushargg-gif@users.noreply.github.com>
2023-07-04 18:04:11 +0530

    Create rsources.php

---
+++ rsources.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PDF Downloads</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
+    <style>
+        body {
+            font-family: 'Roboto', sans-serif;
+            background-color: #f2f2f2;
+            padding: 20px;
+        }
+
+        .page-header {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+
+        .page-title {
+            font-size: 32px;
+            font-weight: 700;
+            color: #333;
+            margin-bottom: 10px;
+        }
+
+        .page-description {
+            font-size: 18px;
+            color: #666;
+            margin-bottom: 20px;
+        }
+
+        .pdf-block {
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            margin-bottom: 20px;
+            text-align: center;
+        }
+
+        .pdf-title {
+            font-size: 24px;
+            font-weight: 700;
+            margin-bottom: 10px;
+        }
+
+        .pdf-description {
+            font-size: 14px;
+            color: #666;
+            margin-bottom: 10px;
+        }
+
+        .pdf-download-link {
+            display: inline-block;
+            background-color: #007bff;
+            color: #fff;
+            text-decoration: none;
+            font-size: 14px;
+            padding: 10px 20px;
+            border-radius: 4px;
+            transition: background-color 0.3s ease;
+        }
+
+        .pdf-download-link:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1 class="page-title">PDF Downloads</h1>
+        <p class="page-description">Welcome to our resource page where you can find and download various PDF documents.</p>
+    </div>
+
+    <div class="pdf-block">
+        <h2 class="pdf-title">PDF 1</h2>
+        <p class="pdf-description">Description of PDF 1</p>
+        <a class="pdf-download-link" href="path/to/pdf1.pdf" download>Download PDF 1</a>
+    </div>
+
+    <div class="pdf-block">
+        <h2 class="pdf-title">PDF 2</h2>
+        <p class="pdf-description">Description of PDF 2</p>
+        <a class="pdf-download-link" href="path/to/pdf2.pdf" download>Download PDF 2</a>
+    </div>
+
+    <div class="pdf-block">
+        <h2 class="pdf-title">PDF 3</h2>
+        <p class="pdf-description">Description of PDF 3</p>
+        <a class="pdf-download-link" href="path/to/pdf3.pdf" download>Download PDF 3</a>
+    </div>
+
+    <!-- Add more PDF blocks as needed -->
+
+</body>
+</html>
